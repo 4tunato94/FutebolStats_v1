9ecbe08d412f9792aaa3dee545474bbbaf124028
4tunato94 <danielisfortunato@gmail.com>
2025-08-02 20:29:52 -0300

    Corrigir conexão com Expo Go

--- metro.config.js
+++ metro.config.js
@@ -2,9 +2,4 @@ const { getDefaultConfig } = require('expo/metro-config');
 
 const config = getDefaultConfig(__dirname);
 
-// Ensure proper resolution for React Native Web
-config.resolver.alias = {
-  'react-native': 'react-native-web',
-};
-
 module.exports = config;
