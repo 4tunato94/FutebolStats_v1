4fce5f19d820ae0bc958e77f949c44f1319040ef
4tunato94 <danielisfortunato@gmail.com>
2025-08-02 20:32:54 -0300

    Corrigir problemas de preview e Expo

--- metro.config.js
+++ metro.config.js
@@ -2,4 +2,7 @@ const { getDefaultConfig } = require('expo/metro-config');
 
 const config = getDefaultConfig(__dirname);
 
+// Ensure compatibility with Expo Router
+config.resolver.assetExts.push('png', 'jpg', 'jpeg', 'svg');
+
 module.exports = config;
